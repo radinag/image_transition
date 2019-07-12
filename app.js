6f6b68958a5e1be9ddec2aa5902980c81b010d33
Radina <49740981+radinag@users.noreply.github.com>
2019-07-11 23:27:12 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+new hoverEffect({
+    parent: document.querySelector('.distortion'),
+    intensity: 0.3,
+    image1: 'ballet2.jpg',
+    image2: 'ballet1.jpg',
+    displacementImage: 'distort.png'
+    //angle: Math.PI / 8,
+});
